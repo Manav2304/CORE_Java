c0cd198267470817c883163277240547aa79f269
Manav patel <manavpatel232002@gmail.com>
2026-03-25 18:53:09 -0400

    feat: created arrays with enhanced for loop

--- src/Arrays.java
+++ src/Arrays.java
@@ -13,5 +13,23 @@ public class Arrays {
         for(int i=0; i<5 ; i++){
             System.out.println(a[i]);
         }
+        ForEnhanced.show();
+    }
+}
+//this is the example in which I have created one more class for the enhanced for loop
+class ForEnhanced{
+    public static void show(){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter numbers");
+
+        int[] a= new int[5];
+        for (int i=0; i <5; i++ ){
+            a[i] = sc.nextInt();
+        }
+            System.out.println("numbers");
+        //below for loop is the enhanced for loop
+        for (int i : a){
+            System.out.println(i);
+        }
     }
 }
