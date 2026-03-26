2a6f95fc1bb5f715e7e2d18f305b1eacb7b9d349
Manav patel <manavpatel232002@gmail.com>
2026-03-26 18:37:24 -0400

    feat: used Random predefined function

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,19 +1,22 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Arrays {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        int[ ] a = new int[5];
-        System.out.println("Enter 5 numbers");
-        for(int i=0;i<5;i++){
-            a[i]=sc.nextInt();
-        }
-        System.out.println("These are the numbers that entered by the user");
-        for(int i=0; i<5 ; i++){
-            System.out.println(a[i]);
-        }
-        ForEnhanced.show();
+//        Scanner sc = new Scanner(System.in);
+//        int[ ] a = new int[5];
+//        System.out.println("Enter 5 numbers");
+//        for(int i=0;i<5;i++){
+//            a[i]=sc.nextInt();
+//        }
+//        System.out.println("These are the numbers that entered by the user");
+//        for(int i=0; i<5 ; i++){
+//            System.out.println(a[i]);
+//        }
+//        ForEnhanced.show();
+//        length.LENGTH();
+        RandomValues.RandomClass();
     }
 }
 //this is the example in which I have created one more class for the enhanced for loop
@@ -29,6 +32,33 @@ class ForEnhanced{
             System.out.println("numbers");
         //below for loop is the enhanced for loop
         for (int i : a){
+            System.out.println(i);
+        }
+    }
+}
+
+class length{
+    public static void LENGTH(){
+    int[] a= new int[20];
+    for (int i=0; i<a.length; i++){
+        a[i]=i+1;
+    }
+    for (int i : a){
+
+        System.out.println(i);
+    }
+    }
+}
+
+class RandomValues{
+    public static void RandomClass(){
+        Random r = new Random();
+        int[] a = new int[20];
+        for (int i = 0 ; i < a.length; i++){
+            a[i] = r.nextInt(50);
+
+        }
+        for(int i : a){
             System.out.println(i);
         }
     }
