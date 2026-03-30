9cefd859c9ba1edc899fbb767d0582c14a55b18a
Manav patel <manavpatel232002@gmail.com>
2026-03-30 13:31:30 -0400

    created jagged array

--- src/Arrays.java
+++ src/Arrays.java
@@ -17,7 +17,9 @@ public class Arrays {
 //        ForEnhanced.show();
 //        length.LENGTH();
 //        RandomValues.RandomClass();
-        Arraytwodimenstional.Arraytwodimentional();
+//        Arraytwodimenstional.Arraytwodimentional();
+//        EnhancedForLoop.EnhancedFor();
+        JaggedArray.Jaggedarray();
     }
 }
 //this is the example in which I have created one more class for the enhanced for loop
@@ -90,5 +92,49 @@ class Arraytwodimenstional{
             System.out.println(" ");
         }
 
+    }
+}
+
+class EnhancedForLoop{
+    public static void EnhancedFor(){
+        int[] nums= new int[10];
+        Random r=new Random();
+        for (int i = 0 ; i<10; i++){
+            nums[i] = r.nextInt(50);
+        }
+        for (int n : nums){
+            System.out.println(n);
+        }
+    }
+}
+class EnhancedForLoopfortwoDarray{
+    public static void EnhancedFor(){
+        int[] nums= new int[10];
+        Random r=new Random();
+        for (int i = 0 ; i<10; i++){
+            nums[i] = r.nextInt(50);
+        }
+        for (int n : nums){
+            System.out.println(n);
+        }
+    }
+}
+class JaggedArray{
+    public static void Jaggedarray(){
+        int a=5, b=6,c=7,d=8;
+        int[] x = {5,6,7,8};
+        int[] y = {6,7};
+        int[] z = {9,8,7};
+        int[][] p ={
+                {5,6,7,8},
+                {6,7},
+                {9,8,7}
+        };
+    for (int i[]:p){
+        for (int j:i){
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
     }
 }
