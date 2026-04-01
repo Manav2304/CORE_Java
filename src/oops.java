acad357f0385223ba0fffa6a9f99480554495817
Manav patel <manavpatel232002@gmail.com>
2026-03-31 22:43:16 -0400

    feat: added constructor overloading

--- src/oops.java
+++ src/oops.java
@@ -1,12 +1,19 @@
 public class oops {
     static public void main(String ... args){
-        Abc obj = new Abc();
+        Abc obj = new Abc(5);
+
 
     }
 }
 
+//below exaample state that constructor overloading
 class Abc{
     public Abc()
     {
         System.out.println(" In Abc constructor");
-    }}
+    }
+    public Abc(int a ){
+        System.out.println(" In Abc constructor with parameter ");
+
+    }
+}
