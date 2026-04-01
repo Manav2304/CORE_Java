394ea72de955ca1fe200e55f90997b322397d49b
Manav patel <manavpatel232002@gmail.com>
2026-03-31 22:39:48 -0400

    feat: added constructor

--- src/Arrays.java
+++ src/Arrays.java
@@ -2,7 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Arrays {
-    public static void main(String[] args) {
+    public static void main(String ... args) {
 
 //        Scanner sc = new Scanner(System.in);
 //        int[ ] a = new int[5];
---
+++ src/oops.java
@@ -0,0 +1,12 @@
+public class oops {
+    static public void main(String ... args){
+        Abc obj = new Abc();
+
+    }
+}
+
+class Abc{
+    public Abc()
+    {
+        System.out.println(" In Abc constructor");
+    }}
