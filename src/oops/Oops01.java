87c215b737ec2af7d258d5ca0fe9045a4194844d
Manav patel <manavpatel232002@gmail.com>
2026-06-23 00:21:38 -0400

    learn: how to create a class

--- src/oops/Oops01.java
+++ src/oops/Oops01.java
@@ -1,11 +1,11 @@
 class Calculator {
     public int add(int a, int b) {
-        return a + b;
+        int sum=a+b;
+        return sum;
     }
 
     
 }
-
 
 
 public class Oops01 {
@@ -14,8 +14,9 @@ public class Oops01 {
         int num1 = 10;
         int num2 = 20;
         Calculator c1 = new Calculator();
-        int result = c1.add(num1, num2);
+        int result = c1.add(num1 , num2);
         System.out.println("Result: " + result);
 
     }
 }
+
